63b14c5f6857936c067d17f1e286d22ec03b34fc
Ekaterina <ekaterina.korobeinikova@gmail.com>
2023-11-02 21:31:11 +0300

    Добавила read_from_file.py

---
+++ read_from_file.py
@@ -0,0 +1,15 @@
+f = open('users.txt', 'r')
+
+for line in f:
+    line = line.strip()
+    split_line = line.split(':')
+    #print(split_line)
+    name = split_line[0]
+    surname = split_line[1]
+    sex = split_line[2]
+    phone = split_line[3]
+    print(f'Привет. Меня зовут {name}, моя фамилия {surname}, пол {sex}. Мой номер телефона: {phone}')
+f.close()
+#insert into telsprav values('kirill','morozov','m','89110271345')
+for strings in f:
+    insert_string = (name, surname, sex, phone)
